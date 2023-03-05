64d73203d7699697d34557daf1dd7088f53969e3
Alka Nixon <20100677@mail.wit.ie>
2023-03-05 16:29:04 +0000

    Arrays.

--- lab001/fundamentals.js
+++ lab001/fundamentals.js
@@ -1,5 +1,16 @@
 const aCar = {
     owner : "Joe Bloggs",
+    address : '3 Walkers Lane',
+    previous_owners: [
+      {
+        name: 'Pat Smith',
+        address: '1 Main Street'
+      },
+      {
+        name: 'Sheila Dwyer',
+        address: '2 High Street'
+      }
+    ],
     type : {
       make: 'Toyota',
       model: 'Corolla',
@@ -22,5 +33,6 @@ const aCar = {
   }
   
   console.log(aCar.owner + ' drives a ' + aCar.type.make)
+  console.log('First owner : ' + aCar.previous_owners[0].name + ' - ' + aCar.previous_owners[0].address)
   console.log('Reg. = ' + aCar.registration.year + '-' + aCar.registration.country_code + '-' + aCar.registration.number)
   console.log('It is a ' + aCar.colour.exterior + ' car, ' + aCar.mileage + ' mileage, with ' + aCar.colour.interior.texture + ' interior.')
