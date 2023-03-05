8b0d8aca999eaa78d6d380c39070d6e8d94d770b
Alka Nixon <20100677@mail.wit.ie>
2023-03-05 15:50:15 +0000

    Dynamic properties.

--- lab001/fundamentals.js
+++ lab001/fundamentals.js
@@ -11,6 +11,16 @@ const aCar = {
       number: '1058'
     }
   };
+
+  aCar.mileage = 10000;
+  aCar.colour = {
+    exterior: 'red',
+    interior: {
+      texture: 'leather',
+      shade: 'cream'
+    }
+  }
   
   console.log(aCar.owner + ' drives a ' + aCar.type.make)
   console.log('Reg. = ' + aCar.registration.year + '-' + aCar.registration.country_code + '-' + aCar.registration.number)
+  console.log('It is a ' + aCar.colour.exterior + ' car, ' + aCar.mileage + ' mileage, with ' + aCar.colour.interior.texture + ' interior.')
