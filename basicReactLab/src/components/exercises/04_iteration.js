2ab71ea99802efc14a5a36423129c1d5f196decd
Alka Nixon <20100677@mail.wit.ie>
2023-04-07 21:46:20 +0100

    Exercise 4 completed.

--- basicReactLab/src/components/exercises/04_iteration.js
+++ basicReactLab/src/components/exercises/04_iteration.js
@@ -1,10 +1,30 @@
 import React from "react";
 import "../../../node_modules/bootstrap/dist/css/bootstrap.css";
 
-
-const Demo = () => {
+const Demo = (props) => {
+  const rows = props.modules.map((module, index) => {
+    return (
+      <tr key={index}>
+        <td>{module.name}</td>
+        <td>{module.noLectures}</td>
+        <td>{module.noPracticals}</td>
+      </tr>
+    );
+  });
   return (
-      <h1>TODO</h1>
+    <div>
+      <h2>{props.course}</h2>
+      <table className="table table-bordered">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>No lectures</th>
+            <th>No practicals</th>
+          </tr>
+        </thead>
+        <tbody>{rows}</tbody>
+      </table>
+    </div>
   );
 };
 
