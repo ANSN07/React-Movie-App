5f5dcd8e16a987c1c205ce19746c38b034d9e8e3
Alka Nixon <20100677@mail.wit.ie>
2023-04-10 14:49:03 +0100

    Add new review support.

--- labMoviesApp/src/index.jsx
+++ labMoviesApp/src/index.jsx
@@ -10,6 +10,7 @@ import SiteHeader from "./components/siteHeader";
 import { QueryClientProvider, QueryClient } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 import MoviesContextProvider from "./contexts/moviesContext";
+import AddMovieReviewPage from "./pages/addMovieReviewPage";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -28,9 +29,13 @@ const App = () => {
         <SiteHeader />
         <MoviesContextProvider>
           <Routes>
+            <Route path="/reviews/form" element={<AddMovieReviewPage />} />
             <Route path="/movies/upcoming" element={<UpcomingMoviesPage />} />
             <Route path="/reviews/:id" element={<MovieReviewPage />} />
-            <Route path="/movies/favourites" element={<FavouriteMoviesPage />} />
+            <Route
+              path="/movies/favourites"
+              element={<FavouriteMoviesPage />}
+            />
             <Route path="/movies/:id" element={<MoviePage />} />
             <Route path="/" element={<HomePage />} />
             <Route path="*" element={<Navigate to="/" />} />
