ca60ca9e81fe674268360820ca63706fa31a74e1
Alka Nixon <20100677@mail.wit.ie>
2023-04-11 16:36:43 +0100

    Refactor: Template movie category page

---
+++ labMoviesApp/src/components/templateMovieCategory/index.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import PageTemplate from "../templateMovieListPage";
+import Spinner from "../spinner";
+import useFiltering from "../../hooks/useFiltering";
+import { useQuery } from "react-query";
+import MovieFilterUI, { titleFilter, genreFilter } from "../movieFilterUI";
+
+const titleFiltering = {
+  name: "title",
+  value: "",
+  condition: titleFilter,
+};
+const genreFiltering = {
+  name: "genre",
+  value: "0",
+  condition: genreFilter,
+};
+
+const TemplateMovieCategoryPage = ({ title, keyValue, api, action }) => {
+  const { data, error, isLoading, isError } = useQuery(keyValue, api);
+  const { filterValues, setFilterValues, filterFunction } = useFiltering(
+    [],
+    [titleFiltering, genreFiltering]
+  );
+
+  if (isLoading) {
+    return <Spinner />;
+  }
+  if (isError) {
+    return <h1>{error.message}</h1>;
+  }
+
+  const changeFilterValues = (type, value) => {
+    const changedFilter = { name: type, value: value };
+    const updatedFilterSet =
+      type === "title"
+        ? [changedFilter, filterValues[1]]
+        : [filterValues[0], changedFilter];
+    setFilterValues(updatedFilterSet);
+  };
+
+  const movies = data ? data.results : [];
+  const displayedMovies = filterFunction(movies);
+
+  return (
+    <>
+      <PageTemplate title={title} movies={displayedMovies} action={action} />
+      <MovieFilterUI
+        onFilterValuesChange={changeFilterValues}
+        titleFilter={filterValues[0].value}
+        genreFilter={filterValues[1].value}
+      />
+    </>
+  );
+};
+export default TemplateMovieCategoryPage;
--- labMoviesApp/src/pages/upcomingMoviesPage.jsx
+++ labMoviesApp/src/pages/upcomingMoviesPage.jsx
@@ -1,68 +1,18 @@
 import React from "react";
-import PageTemplate from "../components/templateMovieListPage";
 import { getUpcomingMovies } from "../api/tmdb-api";
-import Spinner from "../components/spinner";
-import useFiltering from "../hooks/useFiltering";
-import { useQuery } from "react-query";
-import MovieFilterUI, {
-  titleFilter,
-  genreFilter,
-} from "../components/movieFilterUI";
+import TemplateMovieCategoryPage from "../components/templateMovieCategory";
 import AddToPlaylistIcon from "../components/cardIcons/addToPlaylist";
 
-const titleFiltering = {
-  name: "title",
-  value: "",
-  condition: titleFilter,
-};
-const genreFiltering = {
-  name: "genre",
-  value: "0",
-  condition: genreFilter,
-};
-
 const UpcomingMoviesPage = (props) => {
-  const { data, error, isLoading, isError } = useQuery(
-    "upcoming",
-    getUpcomingMovies
-  ); // Caching used in upcoming movies page.
-  const { filterValues, setFilterValues, filterFunction } = useFiltering(
-    [],
-    [titleFiltering, genreFiltering]
-  );
-
-  if (isLoading) {
-    return <Spinner />;
-  }
-  if (isError) {
-    return <h1>{error.message}</h1>;
-  }
-
-  const changeFilterValues = (type, value) => {
-    const changedFilter = { name: type, value: value };
-    const updatedFilterSet =
-      type === "title"
-        ? [changedFilter, filterValues[1]]
-        : [filterValues[0], changedFilter];
-    setFilterValues(updatedFilterSet);
-  };
-
-  const movies = data ? data.results : [];
-  const displayedMovies = filterFunction(movies);
-
   return (
     <>
-      <PageTemplate
+      <TemplateMovieCategoryPage
         title="Upcoming Movies"
-        movies={displayedMovies}
+        keyValue={"upcoming"}
+        api={getUpcomingMovies}
         action={(movie) => {
           return <AddToPlaylistIcon movie={movie} />;
         }}
-      />
-      <MovieFilterUI
-        onFilterValuesChange={changeFilterValues}
-        titleFilter={filterValues[0].value}
-        genreFilter={filterValues[1].value}
       />
     </>
   );
